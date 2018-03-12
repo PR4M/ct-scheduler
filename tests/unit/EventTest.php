4d97a3db09c19ff5b18a8ab9ed65c1cc51bddced
pramadiputra <pramadiputra@outlook.com>
2018-03-12 18:43:52 +0800

    Adding first dummy Simple Test
    Adding first dummy simple test to check whether the phpunit has been properly plugged

---
+++ tests/unit/EventTest.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Pramana
+ * Date: 3/12/2018
+ * Time: 6:36 PM
+ */
+
+class EventTest extends \PHPUnit\Framework\TestCase
+{
+    public function test_sample_test()
+    {
+        $this->assertTrue(true);
+    }
+}
