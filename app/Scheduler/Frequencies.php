cfdd91d6f388d12d950faad143ae12a3a68be79a
pramadiputra <pramadiputra@outlook.com>
2018-03-12 22:54:00 +0800

    Adding replaceIntoExpression to support Chaining
    And also given return $this on every function

--- app/Scheduler/Frequencies.php
+++ app/Scheduler/Frequencies.php
@@ -14,20 +14,38 @@ trait Frequencies
     public function cron($expression)
     {
         $this->expression = $expression;
+
+        return $this;
     }
 
     public function everyMinute()
     {
         $this->expression = '* * * * *';
+
+        return $this;
     }
 
     public function everyTenMinutes()
     {
         $this->expression = '*/10 * * * *';
+
+        return $this;
     }
 
     public function everyThirtyMinutes()
     {
         $this->expression = '*/30 * * * *';
+
+        return $this;
+    }
+
+    public function replaceIntoExpression($position, $value)
+    {
+        $value = (array)$value;
+
+        $expression = explode(' ', $this->expression);
+        array_splice($expression, $position - 1, 1, $value);
+
+        return $this->cron(implode(' ', $expression));
     }
 }
