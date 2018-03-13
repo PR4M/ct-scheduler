1549d1c313377feab95064f28993be3772b1c928
pramadiputra <pramadiputra@outlook.com>
2018-03-13 08:59:57 +0800

    Prepared Kernel to run

--- app/Scheduler/Kernel.php
+++ app/Scheduler/Kernel.php
@@ -9,9 +9,14 @@
 namespace App\Scheduler;
 
 
+use Carbon\Carbon;
+use phpDocumentor\Reflection\Types\This;
+
 class Kernel
 {
     protected $events = [];
+
+    protected $date;
 
     public function getEvents()
     {
@@ -21,5 +26,19 @@ class Kernel
     public function add(Event $event)
     {
         $this->events[] = $event;
+    }
+
+    public function setDate(Carbon $date)
+    {
+        $this->date = $date;
+    }
+
+    public function getDate()
+    {
+        if(!$this->date) {
+            return Carbon::now();
+        }
+        
+        return $this->date;
     }
 }
--- tests/unit/KernelTest.php
+++ tests/unit/KernelTest.php
@@ -6,6 +6,7 @@
  * Time: 8:39 AM
  */
 
+use Carbon\Carbon;
 use App\Scheduler\Kernel;
 use App\Scheduler\Event;
 
@@ -37,5 +38,23 @@ class KernelTest extends \PHPUnit\Framework\TestCase
 
         $kernel = new Kernel;
         $kernel->add('is not event');
+    }
+
+    /** @test */
+    public function can_set_date()
+    {
+        $kernel = new Kernel;
+        $kernel->setDate(Carbon::now());
+
+        $this->assertInstanceOf(Carbon::class, $kernel->getDate());
+    }
+
+    /** @test */
+    public function has_default_date_set()
+    {
+        $kernel = new Kernel;
+        $kernel->setDate(Carbon::now());
+
+        $this->assertInstanceOf(Carbon::class, $kernel->getDate());
     }
 }
